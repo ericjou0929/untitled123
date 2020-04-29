64dbc82a92a78fbfc04bdc44e6a27333a821d3b8
LE-S28 <a0963327198@gmail.com>
2020-04-29 19:24:18 +0800

    123

--- ericjou/ForLoopDemo4.py
+++ ericjou/ForLoopDemo4.py
@@ -2,3 +2,11 @@ e1 = {'name': 'John', 'salary': 50000, 'program ' : ['Jave', 'Python']}
 e2 = {'name': 'Mary', 'salary': 60000,'program ' : ['VB', 'Python']}
 e3 = {'name': 'Bob', 'salary': 70000,'program ' : ['Python']}
 emps = [e1, e2, e3]
+# 求會 Python 的人名
+names=[]
+lan
+ for emp in emps:
+      for p in emp['program']:
+         names.append(emp['name'])
+
+print(names)

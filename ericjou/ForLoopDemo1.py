9570b46f79d90dbcdf1381ea8f73bbb3e2895493
LE-S28 <a0963327198@gmail.com>
2020-04-29 20:00:15 +0800

    123

---
+++ ericjou/ForLoopDemo1.py
@@ -0,0 +1,16 @@
+# Dict (字典結構)
+#
+# salary = {'Jonh': 50000, 'Mary': 60000}
+# print(salary)
+# print(salary['John']) #取得John 的薪資
+# for name in salary
+#     print("%s 的薪資 %d" % (name, salary[name]))
+
+
+
+    # dict 字典結構
+    salary = {'John': 50000, 'Mary': 60000}
+    print(salary)
+    print(salary['John'])  # 取得 John 的薪資
+    for name in salary:
+        print("%s 的薪資 %d" % (name, salary[name]))

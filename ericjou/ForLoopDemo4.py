f1f78b264c62b8ab1f03b1bc29751c76911de33c
LE-S28 <a0963327198@gmail.com>
2020-04-29 19:14:59 +0800

    123

---
+++ ericjou/ForLoopDemo4.py
@@ -0,0 +1,4 @@
+e1 = {'name': 'John', 'salary': 50000, 'program ' : ['Jave', 'Python']}
+e2 = {'name': 'Mary', 'salary': 60000,'program ' : ['VB', 'Python']}
+e3 = {'name': 'Bob', 'salary': 70000,'program ' : ['Python']}
+emps = [e1, e2, e3]

9d72582a1c7a799804dd3809c86440e7f6a1922f
LE-S28 <a0963327198@gmail.com>
2020-04-29 20:00:29 +0800

    123

--- ericjou/ForLoopDemo4.py
+++ ericjou/ForLoopDemo4.py
@@ -1,12 +1,27 @@
-e1 = {'name': 'John', 'salary': 50000, 'program ' : ['Jave', 'Python']}
-e2 = {'name': 'Mary', 'salary': 60000,'program ' : ['VB', 'Python']}
-e3 = {'name': 'Bob', 'salary': 70000,'program ' : ['Python']}
+# # e1 = {'name': 'John', 'salary': 50000, 'program ' : ['Jave', 'Python']}
+# e2 = {'name': 'Mary', 'salary': 60000,'program ' : ['VB', 'Python']}
+# e3 = {'name': 'Bob', 'salary': 70000,'program ' : ['Python']}
+# emps = [e1, e2, e3]
+# # 求會 Python 的人名
+# names=[]
+# lan
+#  for emp in emps:
+#     for p in emp['program']:
+#          names.append(emp['name'])
+#
+# print(names)
+
+
+e1 = {'name': 'John', 'salary': 50000, 'program': ['Java', 'Python']}
+e2 = {'name': 'Mary', 'salary': 60000, 'program': ['VB', 'Python']}
+e3 = {'name': 'Bob', 'salary': 70000, 'program': ['C#']}
 emps = [e1, e2, e3]
-# 求會 Python 的人名
-names=[]
-lan
- for emp in emps:
-      for p in emp['program']:
-         names.append(emp['name'])
+# 求會 Python 的人名 ?
+names = []
+lan = 'Python'
+for emp in emps:
+    for p in emp['program']:
+        if p == lan:
+            names.append(emp['name'])
 
 print(names)
